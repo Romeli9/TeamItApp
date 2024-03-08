3adf45fefcc3e4ae63ad4b6f214b2c643e7902b5
Romeli <butus.5@outlook.com>
2024-03-09 00:04:54 +0700

    navbar ready

--- App.tsx
+++ App.tsx
@@ -1,42 +1,115 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
 import Home from 'pages/Home';
 import { useEffect, useState } from 'react';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './FireBaseConfig';
+import Profile from 'pages/Profile';
+import Messenger from 'pages/Messenger';
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Profile"
+      screenOptions={{
+        tabBarActiveTintColor: '#FFFFFF',
+      }}
+    >
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size, focused  }) => (
+            <View style={[styles.tabIcon, { backgroundColor: focused ? '#9D69DE' : '#FFFFFF' }]}>
+              <MaterialCommunityIcons name="account" color={focused ? '#FFFFFF' : '#BBBBBB'} size={size} />
+            </View>
+          ),
+          tabBarLabel: '',
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => (
+            <View style={[styles.tabIcon, { backgroundColor: focused ? '#9D69DE' : '#FFFFFF' }]}>
+              <MaterialCommunityIcons name="home" color={focused ? '#FFFFFF' : '#BBBBBB'} size={size} />
+            </View>
+          ),
+          tabBarLabel: '',
+        }}
+      />
+      <Tab.Screen
+        name="Messenger"
+        component={Messenger}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size, focused }) => (
+            <View style={[styles.tabIcon, { backgroundColor: focused ? '#9D69DE' : '#FFFFFF' }]}>
+              <MaterialCommunityIcons name="email" color={focused ? '#FFFFFF' : '#BBBBBB'} size={size} />
+            </View>
+          ),
+          tabBarLabel: '',
+        }}
+      />
+      
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   const [user, setUser] = useState<User | null>(null);
 
-  useEffect(()  => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
       console.log('user', user);
       setUser(user);
     });
+
+    return unsubscribe;
   }, []);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName={user ? 'Homepage' : 'Login'}>
         {user ? (
-          <Stack.Screen name='Homepage' component={Home} />
+          <Stack.Screen name='Homepage' component={MyTabs} options={{ headerShown: false }} />
         ) : (
           <>
             <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
-            <Stack.Screen name='RegisterPage' component={Register} />
+            <Stack.Screen name='RegisterPage' component={Register} options={{ headerShown: false }} />
           </>
-          
         )}
-        
-        
       </Stack.Navigator>
       <StatusBar style='auto' />
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabIcon: {
+    marginTop: 30,
+    width: 74,
+    height: 39,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/pages/Messenger.tsx
+++ src/pages/Messenger.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Profile() {
+export default function Messenger() {
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>Messenger</Text>
     </View>
   )
 }
