dd4ca4f1b3c844b3389f51ab2c9729cc1c9086b8
Romeli <butus.5@outlook.com>
2024-03-11 08:16:49 +0700

    fonts added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Button, Modal, TextInput, ScrollView, TouchableOpacity, Image, Alert } from 'react-native';
+import { View, Text, StyleSheet, Modal, TextInput, TouchableOpacity, Image } from 'react-native';
 import { FIREBASE_AUTH, FIREBASE_DB } from '../../FireBaseConfig';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -7,17 +7,16 @@ import { collection, doc, getDoc, setDoc, addDoc, getDocs } from 'firebase/fires
 import ProjectWidget from '../widgets/ProjectWidget';
 import ImagePicker, { ImagePickerResponse } from 'react-native-image-picker'; // Import ImagePickerResponse
 
+
+import { loadFonts } from '../shared/fonts/fonts';
+
+
 const Home: React.FC<{ navigation: any }> = ({ navigation }) => {
   
   const [username, setUsername] = useState<string | null>(null);
   const [userImgUrl, setUserImgUrl] = useState<string | null>(null);
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
-  const [projectName, setProjectName] = useState<string>('');
-  const [projectDescription, setProjectDescription] = useState<string>('');
-  const [projectImage, setProjectImage] = useState<string | null>(null);
-  const [criteria, setCriteria] = useState<string[]>([]);
-  const [categories, setCategories] = useState<string[]>([]);
-  const [projects, setProjects] = useState<any[]>([]);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, async (user) => {
@@ -32,182 +31,117 @@ const Home: React.FC<{ navigation: any }> = ({ navigation }) => {
           setUserImgUrl(userData.ImgUrl || '');
         }
       }
+
+      const loadAppResources = async () => {
+        await loadFonts();
+        setFontsLoaded(true);
+      };
+  
+      loadAppResources();
     });
 
     return unsubscribe;
   }, []);
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const projectsSnapshot = await getDocs(collection(FIREBASE_DB, 'projects'));
-        const loadedProjects = projectsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-        setProjects(loadedProjects);
-      } catch (error) {
-        console.error('Error fetching projects:', error);
-      }
-    };
-
-    fetchProjects();
-  }, []);
-
   const insets = useSafeAreaInsets();
 
-  const pickImage = () => {
-    const options = {
-      title: 'Select Image',
-      storageOptions: {
-        skipBackup: true,
-        path: 'images'
-      }
-    };
-    
-    ImagePicker.showImagePicker(options, (response: ImagePickerResponse) => {
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else {
-        const source = { uri: response.uri };
-        setProjectImage(source.uri);
-      }
-    });
-  };
+  if (!fontsLoaded) {
+    return (
+      <SafeAreaProvider>
+        <View style = {{ flex: 1, paddingTop: insets.top}} >
+          <Text>Loading...</Text>
+        </View>
+      </SafeAreaProvider>
+      
+    );
+  }
+
+  
   
 
-  const createProject = async () => {
-    if (!projectName || !projectDescription || !projectImage || criteria.length === 0 || categories.length === 0) {
-      Alert.alert('Error', 'Please fill all fields');
-      return;
-    }
+  
+  const ModalOpen = () => {
+    setModalVisible(!isModalVisible);
+  } 
 
-    try {
-      const projectData = {
-        name: projectName,
-        description: projectDescription,
-        avatarUrl: projectImage,
-        criteria: criteria,
-        categories: categories,
-        creator: username || '',
-      };
-
-      const projectRef = await addDoc(collection(FIREBASE_DB, 'projects'), projectData);
-      Alert.alert('Success', 'Project created successfully');
-      setModalVisible(false);
-    } catch (error) {
-      console.error('Error creating project:', error);
-      Alert.alert('Error', 'Could not create project');
-    }
+  const ModalClose = () => {
+    setModalVisible(false);
   };
+
+  
 
   return (
     <SafeAreaProvider>
-      <View style={{ flex: 1, paddingTop: insets.top }}>
+      <View style={{ flex: 1, paddingTop: insets.top, backgroundColor: '#FFFFFF', paddingLeft: 16 }}>
         <Text style={{ fontSize: 28 }}>Hello {username}</Text>
-        <ScrollView>
-          {/* Button to open Modal */}
-          <Button title="Create Project" onPress={() => setModalVisible(true)} />
 
-          {/* List of existing projects */}
-          {projects.map((project) => (
-            <ProjectWidget
-              key={project.id}
-              projectName={project.name}
-              projectDescription={project.description}
-              projectAvatarUrl={project.avatarUrl}
-              criteria={project.criteria}
-              categories={project.categories}
-              creator={project.creator}
-              onDelete={() => {}}
-            />
-          ))}
-        </ScrollView>
+          <Text>Ваши проекты:</Text>
+          <TouchableOpacity style={styles.create__button} onPress={ModalOpen}>
+            <Text style={styles.create__text}>+</Text>
+          </TouchableOpacity>
+        
 
-        {/* Modal for creating a new project */}
-        <Modal visible={modalVisible} animationType="slide" transparent={true}>
-          <View style={styles.modalContainer}>
-            <ScrollView>
-              <View style={styles.modalContent}>
-                <TouchableOpacity style={styles.closeButton} onPress={() => setModalVisible(false)}>
-                  <Text style={styles.closeText}>Close</Text>
-                </TouchableOpacity>
-                <Text style={styles.modalTitle}>Create Project</Text>
-                <TextInput
-                  style={styles.input}
-                  placeholder="Project Name"
-                  value={projectName}
-                  onChangeText={(text) => setProjectName(text)}
-                />
-                <TextInput
-                  style={styles.input}
-                  placeholder="Project Description"
-                  value={projectDescription}
-                  onChangeText={(text) => setProjectDescription(text)}
-                  multiline={true}
-                />
-                {projectImage ? (
-                  <Image source={{ uri: projectImage }} style={{ width: 200, height: 200, marginBottom: 10 }} />
-                ) : null}
-                <Button title="Pick Image" onPress={pickImage} />
-                <TextInput
-                  style={styles.input}
-                  placeholder="Criteria (comma separated)"
-                  value={criteria.join(', ')}
-                  onChangeText={(text) => setCriteria(text.split(',').map((item) => item.trim()))}
-                />
-                <TextInput
-                  style={styles.input}
-                  placeholder="Categories (comma separated)"
-                  value={categories.join(', ')}
-                  onChangeText={(text) => setCategories(text.split(',').map((item) => item.trim()))}
-                />
-                <Button title="Create Project" onPress={createProject} />
-              </View>
-            </ScrollView>
+          <Modal visible={isModalVisible} animationType="slide" transparent>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Создание проекта</Text>
+            <TouchableOpacity style={styles.closeButton} onPress={ModalClose}>
+              <Text style={styles.closeButtonText}>Закрыть</Text>
+            </TouchableOpacity>
           </View>
-        </Modal>
+        </View>
+      </Modal>
+       
       </View>
     </SafeAreaProvider>
+
+    
   );
 };
 
 export default Home;
 
 const styles = StyleSheet.create({
+  create__button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#D9D9D9',
+    width: 82,
+    height: 120,
+    borderRadius: 20,
+    marginTop: 13,
+    marginBottom: 13,
+  },
+  create__text: {
+    fontSize: 64,
+    color: '#FFFFFF',
+  },
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
   },
   modalContent: {
-    backgroundColor: '#fff',
+    width: 316,
+    height: 730,
+    backgroundColor: 'white',
     padding: 20,
-    borderRadius: 10,
-    width: '80%',
-    maxHeight: '80%',
-    justifyContent: 'center',
+    borderRadius: 30,
     alignItems: 'center',
   },
   modalTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 27,
+    fontFamily: 'Inter-Bold',
     marginBottom: 20,
-  },
-  input: {
-    width: '100%',
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
+    fontWeight: 'bold',
   },
   closeButton: {
-    alignSelf: 'flex-end',
-    marginBottom: 10,
+    marginTop: 10,
   },
-  closeText: {
+  closeButtonText: {
+    fontSize: 16,
     color: 'blue',
-    textDecorationLine: 'underline',
   },
+
 });
