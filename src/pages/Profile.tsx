e82af0abb6ae85edc9cc0b3557ba97146056b263
Kate <kat.sfrnv.4947@gmail.com>
2024-10-08 15:07:55 +0700

    profile page number 2

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -7,6 +7,7 @@ import {
   View,
   Image,
   Alert,
+  FlatList,
 } from 'react-native';
 import React, {useEffect, useState, useCallback} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -29,8 +30,8 @@ const Profile: React.FC<{navigation: any}> = ({navigation}) => {
   const [pickerResponse, setPickerResponse] =
     useState<ImagePicker.ImagePickerResult | null>(null);
 
-  const {userName, aboutMe, avatar} = useSelector((state: RootState) => state.user);
-
+  const {userName, aboutMe, avatar, background} = useSelector((state: RootState) => state.user);
+  
   useEffect(() => {
     const fetchData = async () => {
         const user = FIREBASE_AUTH.currentUser;
@@ -47,7 +48,8 @@ const Profile: React.FC<{navigation: any}> = ({navigation}) => {
                     userId: user.uid,
                     username: userData.username,
                     email: userData.email,
-                    avatar: userData.avatar // Загружаем URL аватарки
+                    avatar: userData.avatar, // Загружаем URL аватарки
+                    background: userData.background,
                 }));
                 dispatch(setProfileData(userData));
                 setUserDocRef(userDoc);
@@ -70,8 +72,6 @@ const Profile: React.FC<{navigation: any}> = ({navigation}) => {
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
-      quality: 1,
-      selectionLimit: 1,
     });
 
     if (!result.canceled) {
@@ -83,7 +83,7 @@ const Profile: React.FC<{navigation: any}> = ({navigation}) => {
         const user = FIREBASE_AUTH.currentUser; // Получаем текущего пользователя
         console.log('Current user for image upload:', user); // Проверка текущего пользователя
         if (user) {
-          dispatch(setUserData({ userId: user.uid, username: userName, email: user.email, avatar: imageUrl }));
+          dispatch(setUserData({ userId: user.uid, username: userName, email: user.email, avatar: imageUrl, background: background }));
           Alert.alert('Изображение загружено!', 'Ваш новый аватар успешно обновлен.');
         } else {
           console.log('User is not signed in while uploading image.');
@@ -92,6 +92,37 @@ const Profile: React.FC<{navigation: any}> = ({navigation}) => {
     }
   }, [dispatch, userName]);
   
+  const backImageLibraryPress = useCallback(async () => {
+    const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('Извините, но нам нужно разрешение на доступ к вашей галерее!');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    if (!result.canceled) {
+      setPickerResponse(result);
+      setSelectedImage(result.assets[0].uri);
+      
+      const imageUrl = await uploadImageToFirebase1(result.assets[0].uri);
+      if (imageUrl) {
+        const user = FIREBASE_AUTH.currentUser; // Получаем текущего пользователя
+        console.log('Current user for image upload:', user); // Проверка текущего пользователя
+        if (user) {
+          dispatch(setUserData({ userId: user.uid, username: userName, email: user.email, avatar: avatar, background: imageUrl }));
+          Alert.alert('Изображение загружено!', 'Ваш новый аватар успешно обновлен.');
+        } else {
+          console.log('User is not signed in while uploading image.');
+        }
+      }
+    }
+  }, [dispatch, userName]);
+
   const uploadImageToFirebase = async (uri: string) => {
     try {
         const response = await fetch(uri);
@@ -116,8 +147,34 @@ const Profile: React.FC<{navigation: any}> = ({navigation}) => {
         console.error('Error uploading image: ', error);
         return null;
     }
-};
+    
+  };
+
+  const uploadImageToFirebase1 = async (uri: string) => {
+    try {
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        const storageRef = ref(FIREBASE_STORAGE, `images/${userName}_${Date.now()}`);
+        
+        // Загружаем изображение в Firebase Storage
+        await uploadBytes(storageRef, blob);
+        
+        // Получаем URL загруженного изображения
+        const url = await getDownloadURL(storageRef);
   
+        // Сохраните URL изображения в Firestore
+        const user = FIREBASE_AUTH.currentUser;
+        if (user) {
+            const userDocRef = doc(collection(FIREBASE_DB, 'users'), user.uid);
+            await setDoc(userDocRef, { background: url }, { merge: true });
+        }
+  
+        return url; // Возвращаем URL для дальнейшего использования
+    } catch (error) {
+        console.error('Error uploading image: ', error);
+        return null;
+    }
+  };
   const handleSignOut = async () => {
     try {
       await FIREBASE_AUTH.signOut();
@@ -126,80 +183,99 @@ const Profile: React.FC<{navigation: any}> = ({navigation}) => {
       console.error('Sign out error:', error);
     }
   };
-  
-
-
-  
-
-  
 
   return (
     <SafeAreaProvider>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.container}>
-        <View style={styles.image_teamIT}>
-        <Image
-            source={require('../assets/teamIt/Case2.png')}
-            style={{width: 150, height: 150}}
-          />
-        </View>
-        <View style={styles.image_teamIT}>
-        <Image
-            source={require('../assets/teamIt/Case1.png')}
-            style={{width: 150, height: 150}}
-          />
-        </View>
-        
-        <TouchableOpacity
-          style={styles.closeButton}
-          onPress={() => setEditProfileVisible(true)}>
-          <Image
-            source={require('../assets/profile/edit.png')}
-            style={{width: 25, height: 25}}
-          />
-        </TouchableOpacity>
-        <View style={styles.container1}>
-          {aboutMe ? (
-            <ProfileInfo />
-          ) : (
-            <View style={styles.profileDataContainer}>
-              <Text>Пожалуйста, заполните свой профиль</Text>
-              <Text>нажмите кнопку справа с верху</Text>
+      
+      <FlatList
+      scrollEnabled={true}
+      keyboardShouldPersistTaps="handled"
+        contentContainerStyle={styles.scrollContainer}
+        data={[{}]} // Используйте пустой массив для отображения заголовка
+        renderItem={() => (
+          <View style={styles.container}>
+            <View style={styles.image_teamIT}>
+              <Image
+                source={require('../assets/teamIt/Case2.png')}
+                style={{ width: 150, height: 150 }}
+              />
             </View>
-          )}
-        </View>
-        <TouchableOpacity
+            <View style={styles.image_teamIT}>
+              <Image
+                source={require('../assets/teamIt/Case1.png')}
+                style={{ width: 150, height: 150 }}
+              />
+            </View>
+
+            
+            <View style={styles.container1}>
+              {aboutMe ? (
+                <ProfileInfo />
+              ) : (
+                <View style={styles.profileDataContainer}>
+                  <Text>Пожалуйста, заполните свой профиль</Text>
+                  <Text>нажмите кнопку справа с верху</Text>
+                </View>
+              )}
+            </View>
+            <TouchableOpacity
+              style={styles.background_image}
+              onPress={backImageLibraryPress}>
+              {background ? (
+                <Image
+                  source={{ uri: background }}
+                  style={styles.selectedBackgroundImage}
+                />
+              ) : (
+                <Text style={styles.add_image__text}>+</Text>
+              )}
+            </TouchableOpacity>
+            <TouchableOpacity
               style={styles.add_image__button}
               onPress={onImageLibraryPress}>
-              {selectedImage ? (
+              {avatar ? (
                 <Image
-                  source={{uri: selectedImage}}
+                  source={{ uri: avatar }}
                   style={styles.selectedImage}
                 />
               ) : (
                 <Text style={styles.add_image__text}>+</Text>
               )}
             </TouchableOpacity>
-        <Text style={styles.text}>@{userName}</Text>
-        
-        <TouchableOpacity
-          style={styles.exitButton}
-          onPress={handleSignOut}>
-          <Image
-            source={require('../assets/profile/exit.png')}
-            style={{width: 25, height: 23}}
-          />
-        </TouchableOpacity>
-        {isEditProfileVisible && (
-          <EditProfile
-            onModalClose={() => setEditProfileVisible(false)}
-            userDocRef={userDocRef}
-          />
+            <Text style={styles.text}>@{userName}</Text>
+
+            
+            {isEditProfileVisible && (
+              <EditProfile
+                onModalClose={() => setEditProfileVisible(false)}
+                userDocRef={userDocRef}
+              />
+            )}
+            <TouchableOpacity
+              style={styles.exitButton}
+              onPress={handleSignOut}>
+              <Image
+                source={require('../assets/profile/exit.png')}
+                style={{ width: 25, height: 23 }}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setEditProfileVisible(true)}>
+              <Image
+                source={require('../assets/profile/edit.png')}
+                style={{ width: 25, height: 25 }}
+              />
+            </TouchableOpacity>
+          </View>
+          
         )}
-      </View>
-      </ScrollView>
+        keyExtractor={(item, index) => index.toString()} // Используйте индекс как ключ
+       
+      />
     </SafeAreaProvider>
   );
+  
 };
 
 export default Profile;
@@ -207,11 +283,10 @@ export default Profile;
 const styles = StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 200,
@@ -260,7 +335,7 @@ const styles = StyleSheet.create({
     top: 40,
     right: 80,
     width: 20,
-    height: 40,
+    height: 325,
     borderRadius: 18,
     alignItems: 'center',
     justifyContent: 'center',
@@ -271,7 +346,7 @@ const styles = StyleSheet.create({
     top: 42,
     right: 50,
     width: 20,
-    height: 40,
+    height: 325,
     borderRadius: 18,
     alignItems: 'center',
     justifyContent: 'center',
@@ -301,9 +376,22 @@ const styles = StyleSheet.create({
     color: '#333',
     fontFamily: 'Inter-Regular',
   },
+  background_image: {
+    position: 'absolute',
+    justifyContent: 'center',
+    backgroundColor: '#BE9DE8',
+    top: -50,
+    width: 500,
+    height: 225,
+    marginTop: 11,
+    marginBottom: 13,
+  },
+  selectedBackgroundImage: {
+    width: 500,
+    height: 225,
+  },
 });
 
-function uploadImageToFirebase(uri: string) {
-  throw new Error('Function not implemented.');
-}
+
+
 
